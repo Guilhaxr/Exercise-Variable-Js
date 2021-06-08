c47dd081e715e137132b58e6cb4058999998efcc
Guilhax <>
2021-06-08 09:13:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+  /*
+
+    🏁 Please take this quick challenge and rename carefully all the variable, and fix this broken code by assigning the correct 
+    variable keyword you have learned in the quest.
+    ex: const name = "David";
+
+  */
+
+  const name = "David";
+  const name2 = "Bruno";
+  var sentence = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+  var img1 = "http://www.go.com/davif.png";
+
+  var city = "San Francisco";
+  let abstract = "CA";
+
+  let pplFollower = 109;
+  let peoplePfollowing = 1500;
+
+
+  var time = new Date();
+
+  /*
+
+    🏁 David is following one more account, increment the total of following account
+
+  */
+
